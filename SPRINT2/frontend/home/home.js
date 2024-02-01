5e8884c6e3cbdaea3a95e93862263f0ae22b74f2
Eetu <ediz.mantyla@hotmail.com>
2024-02-01 20:12:40 +0200

    Myin sieluni tälle päivälle

--- SPRINT2/frontend/home/home.js
+++ SPRINT2/frontend/home/home.js
@@ -1,15 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const loginButton = document.getElementById("loginButton");
-    const usernameInput = document.getElementById("username");
-    const passwordInput = document.getElementById("password");
-    const signUpCloseLink = document.getElementById("signUpCloseLink");
     const signUpLink = document.getElementById("signUpLink");
     const popup = document.getElementById("popup");
-    const popupClose = document.getElementById("popupClose");
     const closeBtn = document.getElementById("close");
-    const closePopBtn = document.getElementById("closePop");
-
-
     loginButton.disabled = true;
 
     function checkInputs() {
@@ -23,16 +15,8 @@ document.addEventListener("DOMContentLoaded", function() {
         popup.style.display = "block";
     });
 
-    signUpCloseLink.addEventListener("click", function() {
-        popupClose.style.display = "block";
-    });
-
     closeBtn.addEventListener("click", function() {
         popup.style.display = "none";
-    });
-
-    closePopBtn.addEventListener("click", function() {
-        popupClose.style.display = "none";
     });
 
     usernameInput.addEventListener("input", checkInputs);
